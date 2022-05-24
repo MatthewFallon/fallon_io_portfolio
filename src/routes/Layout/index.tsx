01ae46eb60dd2b1817f2ad04c8ff8ec696d6c40d
Matthew Fallon <Matthew.b.fallon@gmail.com>
2022-05-23 21:28:15 -0500

    Adding Material Icons and beginning on content creation

--- src/routes/Layout/index.tsx
+++ src/routes/Layout/index.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Link, Toolbar, Typography } from "@mui/material";
+import { AppBar, CssBaseline, Link, Toolbar, Typography } from "@mui/material";
 import nameLogo from "assets/logo-name-only-export.png";
 import { Outlet, Link as RouterLink } from "react-router-dom";
 
@@ -7,12 +7,13 @@ export default function Layout() {
 
     return (
         <>
-            <AppBar color="transparent">
+            <CssBaseline />
+            <AppBar color="inherit">
                 {/* Using Flex as it's the proper situation for using it when you want a lot of auto-flow behavior.*/}
                 <Toolbar sx={{display: "flex", alignItems: "center"}}>
                     <Link to="/" component={RouterLink} sx={{display: "flex", gap: "15px"}} underline="none" color="inherit" variant="h5">
                         <img src={nameLogo} height={52} alt="" />
-                        <Typography variant="h4" sx={{alignSelf: "end", visibility: {sm: "hidden", md: "visible"}}}>Matthew Fallon</Typography>
+                        <Typography variant="h4" sx={{alignSelf: "end", visibility: {xs: "hidden", md: "visible"}}}>Matthew Fallon</Typography>
                     </Link>
                 </Toolbar>
             </AppBar>
